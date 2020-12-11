1f1e75e16ad5f4877ec7de97dfc476ff04b70503
Pascal Kamber <Pascal.Kamber@netree.ch>
2020-12-11 02:34:37 +0100

    comment changed

--- week11/pep/service/remoteServiceTest.js
+++ week11/pep/service/remoteServiceTest.js
@@ -7,18 +7,18 @@ const service = pepServices(`https://localhost:44382/api/pep`, '');
   remoteServiceSuite.add("setup", async assert => {
     assert.is(1, 1);
 
-    service.loadDevelopers( devs => { //promise
-      assert.is(2, devs.length);   //execution later
+    service.loadDevelopers( devs => { // promise
+      assert.is(2, devs.length);   // execution later
     });
 
-    service.loadDevelopers( devs => { //promise
-      service.loadDevelopers( devs2 => { //promise
-        assert.is(devs.length, devs2.length);   //execution later
+    service.loadDevelopers( devs => {
+      service.loadDevelopers( devs2 => { 
+        assert.is(devs.length, devs2.length);   
       });
     });
     
-    service.loadProjects( prjs => { //promise
-      assert.is(2, prjs.length);   //execution later
+    service.loadProjects( prjs => { // promise
+      assert.is(2, prjs.length);   // execution later
     });
 
   });
