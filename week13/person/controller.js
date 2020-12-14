6ee8170ab72455bb315e3daba2d384a01a5083e1
Dierk Koenig <dierk.koenig@canoo.com>
2020-12-14 15:26:39 +0100

    before week 13

---
+++ week13/person/controller.js
@@ -0,0 +1,35 @@
+/**
+ * @module Controllers as shallow wrappers around observables
+ */
+import { ObservableList, Observable }   from "../observable/observable.js";
+import { reset, Person }                from "./person.js"
+
+export { ListController, SelectionController }
+
+const ListController = modelConstructor => {
+
+    const listModel = ObservableList([]); // observable array of models, this state is private
+
+    return {
+        addModel:            () => listModel.add(modelConstructor()),
+        removeModel:         listModel.del,
+        onModelAdd:          listModel.onAdd,
+        onModelRemove:       listModel.onDel,
+    }
+};
+
+const noSelection = reset(Person());
+noSelection.firstname.setQualifier("Person.none.firstname");
+noSelection.lastname .setQualifier("Person.none.lastname");
+
+const SelectionController = model => {
+
+    const selectedModelObs = Observable(model);
+
+    return {
+        setSelectedModel : selectedModelObs.setValue,
+        getSelectedModel : selectedModelObs.getValue,
+        onModelSelected  : selectedModelObs.onChange,
+        clearSelection   : () => selectedModelObs.setValue(noSelection),
+    }
+};
